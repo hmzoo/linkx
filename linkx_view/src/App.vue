14cd53e45296cde6a6f365be0fa41f3dc538c9ac
Ubuntu <ubuntu@sarthe.fr>
2023-05-23 18:17:04 +0200

    ok

--- linkx_view/src/App.vue
+++ linkx_view/src/App.vue
@@ -3,6 +3,19 @@
 </script>
 
 <template>
+<div class="header">
+    <div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
+        <a class="pure-menu-heading" href="">O</a>
+
+        <ul class="pure-menu-list">
+            <li class="pure-menu-item pure-menu-selected"><a href="#" class="pure-menu-link">Home</a></li>
+            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Tour</a></li>
+            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Sign Up</a></li>
+        </ul>
+    </div>
+</div>
+
+
   <div>Cool <a class="pure-button pure-button-primary" href="#"><font-awesome-icon icon="phone" /> A Primary Button</a>
   
   <font-awesome-icon icon="phone" />
